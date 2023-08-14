b8a1a626db882c54cd6481c451095615d5f30f69
Barno <32536886+CarolBarno@users.noreply.github.com>
2023-08-14 23:26:08 +0300

    Update test.py

--- flask_boggle/test.py
+++ flask_boggle/test.py
@@ -7,4 +7,10 @@ from boggle import Boggle
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
+def test_homepage(self):
+    with self.client:
+        
